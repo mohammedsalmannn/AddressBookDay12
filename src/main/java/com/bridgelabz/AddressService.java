c982277c8edf42ab25176924a567cf2f9e117d10
mohammedsalmannn <rmdsalman001@gmail.com>
2021-06-03 14:33:21 +0530

    ReFactor : Refactor the code added Search method

--- src/main/java/com/bridgelabz/AddressService.java
+++ src/main/java/com/bridgelabz/AddressService.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class AddressService implements IAddress {
 
-    ArrayList<AddressGetterSetter> book =new ArrayList<>();
+    ArrayList<AddressGetterSetter> book = new ArrayList<>();
     HashMap<String, String> citydict = new HashMap<String, String>();
     Scanner s = new Scanner(System.in);
 
@@ -28,17 +28,16 @@ public class AddressService implements IAddress {
         Info.setZip(s.next());
         System.out.println("Enter your phone number");
         Info.setPhone(s.next());
-        for(int i =0;i<book.size();i++) {
-            if(book.get(i).getFname().equals(Info.getFname())) {
-                if(book.get(i).getLname().equals(Info.getLname())) {
+        for (int i = 0; i < book.size(); i++) {
+            if (book.get(i).getFname().equals(Info.getFname())) {
+                if (book.get(i).getLname().equals(Info.getLname())) {
                     System.out.println("Duplicate");
                     break;
                 }
-            }
-            else
+            } else
                 book.add(Info);
         }
-        if(book.size()==0)
+        if (book.size() == 0)
             book.add(Info);
 
     }
@@ -47,20 +46,17 @@ public class AddressService implements IAddress {
     @Override
     public void Edit() {
 
-        String temp=null;
+        String temp = null;
         System.out.println("Enter the Phone number of the record u want to Edit");
-        temp=s.next();
-        if(book.isEmpty())
-        {
+        temp = s.next();
+        if (book.isEmpty()) {
             System.out.println("No records to edit");
             return;
         }
 
-        for(int i=0;i<book.size();i++)
-        {
-            if(book.get(i).getPhone().equals(temp))
-            {
-                AddressGetterSetter Info=new AddressGetterSetter();
+        for (int i = 0; i < book.size(); i++) {
+            if (book.get(i).getPhone().equals(temp)) {
+                AddressGetterSetter Info = new AddressGetterSetter();
                 System.out.println(temp);
                 System.out.println("Enter First name:");
                 Info.setFname(s.next());
@@ -88,38 +84,72 @@ public class AddressService implements IAddress {
     @Override
     public void Delete() {
 
-        String temp=null;
+        String temp = null;
         System.out.println("Enter the First name of the record you want to delete");
-        temp=s.next();
-        if(book.isEmpty())
-        {
+        temp = s.next();
+        if (book.isEmpty()) {
             System.out.println("No records to delete");
             return;
         }
-        for(int i=0;i<book.size();i++)
-        {
-            if(book.get(i).getFname().equals(temp))
-            {
+        for (int i = 0; i < book.size(); i++) {
+            if (book.get(i).getFname().equals(temp)) {
                 book.remove(i);
                 break;
             }
         }
     }
+
     @Override
     public void SortCity() {
         Collections.sort(book, AddressGetterSetter.CityComparator);
     }
+
     @Override
     public void DisplayCity() {
 
         Set set = citydict.entrySet();
         Iterator iterator = set.iterator();
-        while(iterator.hasNext()) {
-            Map.Entry mentry = (Map.Entry)iterator.next();
-            System.out.print("City is: "+ mentry.getKey() + " & Name is: ");
+        while (iterator.hasNext()) {
+            Map.Entry mentry = (Map.Entry) iterator.next();
+            System.out.print("City is: " + mentry.getKey() + " & Name is: ");
             System.out.println(mentry.getValue());
         }
     }
 
+    @Override
+    public void Search() {
+        System.out.println("Search Preferance:1.City  2.State");
+        int input = s.nextInt();
+        if (input == 1) {
+            String temp = null;
+            System.out.println("Enter the City whose record u want to display");
+            temp = s.next();
+            if (book.isEmpty()) {
+                System.out.println("No records to Show");
+                return;
+            }
 
+            for (int i = 0; i < book.size(); i++) {
+                if (book.get(i).getCity().equals(temp)) {
+                    System.out.println(book.get(i).getFname());
+                }
+            }
+        } else {
+            String temp = null;
+            System.out.println("Enter the State whose record u want to display");
+            temp = s.next();
+            if (book.isEmpty()) {
+                System.out.println("No records to Show");
+                return;
+            }
+
+            for (int i = 0; i < book.size(); i++) {
+                if (book.get(i).getState().equals(temp)) {
+                    System.out.println(book.get(i).getFname());
+                }
+            }
+        }
+
+
+    }
 }
--- src/main/java/com/bridgelabz/IAddress.java
+++ src/main/java/com/bridgelabz/IAddress.java
@@ -9,4 +9,6 @@ public interface IAddress {
     void SortCity();
 
     void DisplayCity();
+
+    void Search();
 }
